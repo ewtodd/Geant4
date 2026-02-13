10b12da362545e77884d8bb0425440b226ef42f2
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-02-13 09:59:09 -0500

    ..

--- LZ/24keVNeutrons/generator.cc
+++ LZ/24keVNeutrons/generator.cc
@@ -4,7 +4,7 @@ PrimaryGenerator::PrimaryGenerator() {
   G4int numParticles = 1;
   fParticleGun = new G4ParticleGun(numParticles);
 
-  fParticleGun->SetParticleEnergy(1.0 * MeV);
+  fParticleGun->SetParticleEnergy(0.0 * MeV);
   fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., 0.));
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
 }
@@ -18,5 +18,7 @@ void PrimaryGenerator::GeneratePrimaries(G4Event *anEvent) {
   G4ParticleDefinition *cf252 = ionTable->GetIon(98, 252, 0.0);
 
   fParticleGun->SetParticleDefinition(cf252);
+  fParticleGun->SetParticleEnergy(0.0);
+  fParticleGun->SetParticleCharge(0.0);
   fParticleGun->GeneratePrimaryVertex(anEvent);
 }
--- LZ/24keVNeutrons/physics.cc
+++ LZ/24keVNeutrons/physics.cc
@@ -1,27 +1,20 @@
 #include "physics.hh"
 
 PhysicsList::PhysicsList() {
-  // EM Physics
   RegisterPhysics(new G4EmStandardPhysics());
 
-  // Decay Physics (includes radioactive decay)
   RegisterPhysics(new G4DecayPhysics());
 
-  // Radioactive Decay Physics
   RegisterPhysics(new G4RadioactiveDecayPhysics());
 
-  // Hadronic Physics - High Precision for neutrons
   RegisterPhysics(new G4HadronElasticPhysicsHP());
   RegisterPhysics(new G4HadronPhysicsFTFP_BERT_HP());
 
-  // Ion Physics (for fission products)
   RegisterPhysics(new G4IonPhysics());
 
-  // Stopping Physics (for particle capture)
   RegisterPhysics(new G4StoppingPhysics());
 
-  // Optional: Optical Physics if you want scintillation
-  // RegisterPhysics(new G4OpticalPhysics());
+  RegisterPhysics(new G4OpticalPhysics());
 }
 
 PhysicsList::~PhysicsList() {}
